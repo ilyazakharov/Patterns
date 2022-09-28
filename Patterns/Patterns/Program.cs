876830a830ccde08c79c294686b21387cd7b330b
Ilya Zakharov <ilya_z87@mail.ru>
2022-09-28 21:25:57 +0300

    Singleton pattern

--- Patterns/Patterns/Program.cs
+++ Patterns/Patterns/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Patterns for education
+
+using Patterns.Singleton;
+
+/// <summary>
+/// Main program.
+/// </summary>
+internal class Program
+{
+    /// <summary>
+    ///  Main entry to the program.
+    /// </summary>
+    /// <param name="args">Default parameter.</param>
+    private static void Main(string[] args)
+    {
+        TestSingletone();
+    }
+
+    /// <summary>
+    /// Shows the singletone pattern.
+    /// </summary>
+    private static void TestSingletone()
+    {
+        Parallel.For(1, 10, x => Console.WriteLine(Singleton.Instance(x).SingletonNum));
+    }
+}
---
+++ Patterns/Patterns/Singleton/Singleton.cs
@@ -0,0 +1,50 @@
+﻿namespace Patterns.Singleton
+{
+    /// <summary>
+    /// Singleton pattern class.
+    /// </summary>
+    public class Singleton
+    {
+        private static readonly object LockObj = new ();
+        private static volatile Singleton? instance;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Singleton"/> class.
+        /// </summary>
+        /// <param name="singletonNum">Value of the instance.</param>
+        public Singleton(int singletonNum)
+        {
+            this.SingletonNum = singletonNum;
+        }
+
+        /// <summary>
+        /// Gets the value of the instance.
+        /// </summary>
+        public int SingletonNum { get; }
+
+        /// <summary>
+        /// Function to return singleton object.
+        /// </summary>
+        /// <param name="num">Value of the instance.</param>
+        /// <returns>Singleton object.</returns>
+        public static Singleton Instance(int num)
+        {
+            if (instance != null)
+            {
+                return instance;
+            }
+
+            lock (LockObj)
+            {
+                if (instance != null)
+                {
+                    return instance;
+                }
+
+                instance = new Singleton(num);
+            }
+
+            return instance;
+        }
+    }
+}
